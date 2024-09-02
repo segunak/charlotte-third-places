70156e47526c9880c683922e90739cc95c7760bd
Segun <akintundejr4@gmail.com>
2024-09-02 10:48:14 -0400

    feature, not found page not looking dumb

---
+++ charlotte-third-places/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+    return (
+        <div className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <h1 className="text-4xl font-bold">Page Not Found</h1>
+            <p className="mt-4 text-xl">
+                Could not find the requested resource.
+            </p>
+            <div className="mt-6 text-lg">
+                Return <Link href="/" className="text-blue-500 hover:underline" >Home</Link>.
+            </div>
+        </div>
+    )
+}
--- charlotte-third-places/components/mobile-navigation.tsx
+++ charlotte-third-places/components/mobile-navigation.tsx
@@ -3,33 +3,52 @@
 import React from 'react';
 import Link from 'next/link';
 import { Icons } from "@/components/icons"
+import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button";
 
 export function MobileNavigation() {
+  const pathname = usePathname();
+
   return (
     <div className="sm:hidden">
       <nav className="fixed bottom-0 left-0 right-0 bg-background border-t border-border flex justify-around p-4">
         <Link href="/" className="flex flex-col items-center">
           <Button variant="ghost" size="icon">
-            <Icons.home className="h-6 w-6" />
+            {pathname === "/" ? (
+              <Icons.home className="h-6 w-6" />  // Filled icon if active
+            ) : (
+              <Icons.homeOutline className="h-6 w-6" />  // Outline icon if inactive
+            )}
           </Button>
           <span className="text-xs">Home</span>
         </Link>
         <Link href="/map" className="flex flex-col items-center">
           <Button variant="ghost" size="icon">
-            <Icons.map className="h-6 w-6" />
+            {pathname === "/map" ? (
+              <Icons.map className="h-6 w-6" />
+            ) : (
+              <Icons.mapOutline className="h-6 w-6" />
+            )}
           </Button>
           <span className="text-xs">Map</span>
         </Link>
         <Link href="/contribute" className="flex flex-col items-center">
           <Button variant="ghost" size="icon">
-            <Icons.create className="h-6 w-6" />
+            {pathname === "/contribute" ? (
+              <Icons.create className="h-6 w-6" />
+            ) : (
+              <Icons.createOutline className="h-6 w-6" />
+            )}
           </Button>
           <span className="text-xs">Contribute</span>
         </Link>
         <Link href="/about" className="flex flex-col items-center">
           <Button variant="ghost" size="icon">
-            <Icons.infoCircle className="h-6 w-6" />
+            {pathname === "/about" ? (
+              <Icons.infoCircle className="h-6 w-6" />
+            ) : (
+              <Icons.infoCircleOutline className="h-6 w-6" />
+            )}
           </Button>
           <span className="text-xs">About</span>
         </Link>
