08b08e8e712810579a4a6b7934b9c8087f8238c3
Segun <akintundejr4@gmail.com>
2024-09-03 13:50:30 -0400

    fixed

--- charlotte-third-places/components/mobile-navigation.tsx
+++ charlotte-third-places/components/mobile-navigation.tsx
@@ -37,7 +37,7 @@ export function MobileNavigation() {
 
   return (
     <div className="sm:hidden bg-background z-100">
-      <nav className="sticky flex flex-row justify-around p-2 items-center justify-around w-full bottom-0 border-t border-border">
+      <nav className="fixed flex flex-row justify-around p-2 items-center justify-around w-full bottom-0 border-t border-border">
         {navItems.map((item) => (
           <Link href={item.href} key={item.href} className="flex flex-col items-center touch-manipulation">
             {pathname === item.href ? (
