9e8ea3eedc281bfd6a092d249aeb7fb1327f43aa
Segun <akintundejr4@gmail.com>
2024-09-03 12:16:42 -0400

    z index

--- charlotte-third-places/components/mobile-navigation.tsx
+++ charlotte-third-places/components/mobile-navigation.tsx
@@ -36,7 +36,7 @@ export function MobileNavigation() {
   ];
 
   return (
-    <div className="sm:hidden bg-background">
+    <div className="sm:hidden bg-background z-50">
       <nav className="sticky flex flex-row justify-around p-2 items-center justify-around w-full bottom-0 border-t border-border">
         {navItems.map((item) => (
           <Link href={item.href} key={item.href} className="flex flex-col items-center">
--- charlotte-third-places/components/site-header.tsx
+++ charlotte-third-places/components/site-header.tsx
@@ -6,7 +6,7 @@ import { MainNavigation } from "@/components/main-navigation"
 
 export function SiteHeader() {
     return (
-        <header className="bg-background sticky top-0 z-40 w-full border-b">
+        <header className="bg-background sticky top-0 z-50 w-full border-b">
             <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
 
                 <div className="sm:flex gap-6 md:gap-10 items-center">
