40af36958e67c0358e0aa5e04f6ba18f4a3c4cb5
Segun <akintundejr4@gmail.com>
2024-09-03 17:35:48 -0400

    sticky no height

--- charlotte-third-places/components/mobile-navigation.tsx
+++ charlotte-third-places/components/mobile-navigation.tsx
@@ -37,7 +37,7 @@ export function MobileNavigation() {
   ];
 
   return (
-    <nav className="sm:hidden !h-1 !fixed !bottom-0 !left-0 !right-0 bg-background border-t border-border z-100">
+    <nav className="sm:hidden !sticky !bottom-0 !left-0 !right-0 bg-background border-t border-border z-100">
       <div className="flex justify-around items-center h-16">
         {navItems.map((item) => (
           <Link href={item.href} key={item.href} className="flex flex-col items-center">
