b097e435cff553be8cac77ae825cfd4adef5cbee
Segun <akintundejr4@gmail.com>
2024-09-06 19:26:54 -0400

    More padding on mobile

--- charlotte-third-places/app/page.tsx
+++ charlotte-third-places/app/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
   const places = await getPlaces(); // This will use the cached result or fetch fresh data if the cache is stale
 
   return (
-    <section className="container mx-auto py-8 px-4">
+    <section className="container mx-auto py-8 sm:px-4">
       <div className="mb-6">
         <h1 className="text-3xl font-bold mb-4">Explore Charlotte's Third Places</h1>
         <p>
