8a52a116f1c5b175a2e0842254c32ecb6a838b3b
Segun <akintundejr4@gmail.com>
2024-09-06 00:26:09 -0400

    comment

--- charlotte-third-places/app/page.tsx
+++ charlotte-third-places/app/page.tsx
@@ -11,7 +11,6 @@ export default async function HomePage() {
 
   return (
     <section className="container mx-auto py-8">
-      {/* Header Section */}
       <div className="mb-6">
         <h1 className="text-3xl font-bold mb-4">Explore Charlotte's Third Places</h1>
         <p className="text-lg">
