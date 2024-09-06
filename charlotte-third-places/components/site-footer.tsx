e7a5cccc51d9c797bab8a37128752d59164faef5
Segun Akinyemi <akintundejr4@gmail.com>
2024-09-06 10:06:24 -0400

    Update site-footer.tsx to not show on mobile

--- charlotte-third-places/components/site-footer.tsx
+++ charlotte-third-places/components/site-footer.tsx
@@ -4,7 +4,7 @@ export function SiteFooter() {
     const currentYear: number = new Date().getFullYear();
 
     return (
-        <footer className="  border-t">
+        <footer className="hidden md:block py-6 md:px-8 md:py-0 border-t">
             <div className="container flex flex-col items-center justify-between gap-4 md:h-10 md:flex-row">
                 <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
                     Built by{" "}
