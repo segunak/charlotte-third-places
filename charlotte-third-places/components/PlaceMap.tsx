41d1b1780a8ffb7e8078e03653c78af0e6101495
Segun <akintundejr4@gmail.com>
2024-09-16 01:15:08 -0400

    fully working with modal

--- charlotte-third-places/components/PlaceMap.tsx
+++ charlotte-third-places/components/PlaceMap.tsx
@@ -1,18 +1,21 @@
 "use client"
 
 import { Place } from '@/lib/types';
-import { AdvancedMarker, APIProvider, Map, useMapsLibrary } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
+import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+import { PlaceModal } from '@/components/PlaceModal'; // Import your existing PlaceModal component
 
 interface PlaceMapProps {
     places: Array<Place>;
 }
 
 export function PlaceMap({ places }: PlaceMapProps) {
-    const charlotteCityCenter = { lat: 35.23075539296459, lng: -80.83165532446358 } // Middle of Uptown Charlotte 
+    const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
+    const charlotteCityCenter = { lat: 35.23075539296459, lng: -80.83165532446358 }; // Middle of Uptown Charlotte 
 
     return (
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''}>
-            <div className="w-full max-w-4xl h-[80vh] sm:h-[80vh] md:aspect-square mx-auto border border-gray-200 rounded-xl shadow-xl overflow-hidden">
+            <div className="w-full h-[80vh] sm:h-[80vh] md:aspect-square mx-auto border border-gray-200 rounded-xl shadow-xl overflow-hidden">
                 <Map defaultCenter={charlotteCityCenter} defaultZoom={10} mapId='7b49fa8eab9db6c7'>
                     {places.map((place, index) => {
                         const position = {
@@ -25,10 +28,18 @@ export function PlaceMap({ places }: PlaceMapProps) {
                                 key={index}
                                 position={position}
                                 title={place.name}
+                                onClick={() => setSelectedPlace(place)}
                             />
                         );
                     })}
                 </Map>
+
+                {selectedPlace && (
+                    <PlaceModal
+                        place={selectedPlace}
+                        onClose={() => setSelectedPlace(null)}
+                    />
+                )}
             </div>
         </APIProvider>
     );
