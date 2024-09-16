7f9dba70c07a9e416e4e8847429fb7d56144235e
Segun <akintundejr4@gmail.com>
2024-09-16 01:02:58 -0400

    working

--- charlotte-third-places/components/PlaceMap.tsx
+++ charlotte-third-places/components/PlaceMap.tsx
@@ -14,14 +14,20 @@ export function PlaceMap({ places }: PlaceMapProps) {
         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? ''}>
             <div className="w-full max-w-4xl h-[80vh] sm:h-[80vh] md:aspect-square mx-auto border border-gray-200 rounded-xl shadow-xl overflow-hidden">
                 <Map defaultCenter={charlotteCityCenter} defaultZoom={10} mapId='7b49fa8eab9db6c7'>
-                    {places.map((place, index) => (
-                        
-                        <AdvancedMarker
-                            key={index}
-                            position={{ lat: place.latitude, lng: place.longitude }}
-                            title={place.name}
-                        />
-                    ))}
+                    {places.map((place, index) => {
+                        const position = {
+                            lat: Number(place.latitude),
+                            lng: Number(place.longitude)
+                        };
+
+                        return (
+                            <AdvancedMarker
+                                key={index}
+                                position={position}
+                                title={place.name}
+                            />
+                        );
+                    })}
                 </Map>
             </div>
         </APIProvider>
