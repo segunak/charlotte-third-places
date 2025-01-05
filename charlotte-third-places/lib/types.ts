3a9be57d66c0ec9e0aefb65b16d177b533b9f814
Segun Akinyemi <akintundejr4@gmail.com>
2025-01-05 16:45:22 -0500

    Cleaning up organization of filter logic in terms of types for upcoming data changes

--- charlotte-third-places/lib/types.ts
+++ charlotte-third-places/lib/types.ts
@@ -24,7 +24,7 @@ export type Place = {
     lastModifiedDate: string;
 }
 
-// Default sorting option for the application
+// Defines the fields that are available for sorting to users.
 export enum SortField {
     Name = 'name',
     DateAdded = 'createdDate',
@@ -36,7 +36,81 @@ export enum SortDirection {
     Descending = 'desc',
 }
 
+export interface SortOption {
+    field: SortField;
+    direction: SortDirection;
+}
+
 export const DEFAULT_SORT_OPTION = {
     field: SortField.DateAdded,
     direction: SortDirection.Descending,
+};
+
+export interface FilterOption {
+    value: string;
+    placeholder: string;
+    label: string;
+    predefinedOrder: string[];
+}
+
+export interface FilterConfig {
+    name: FilterOption;
+    type: FilterOption;
+    size: FilterOption;
+    neighborhood: FilterOption;
+    purchaseRequired: FilterOption;
+    parkingSituation: FilterOption;
+    freeWifi: FilterOption;
+    hasCinnamonRolls: FilterOption;
+}
+
+export const DEFAULT_FILTER_CONFIG: FilterConfig = {
+    name: {
+        value: "all",
+        placeholder: "Name",
+        label: "Name",
+        predefinedOrder: [],
+    },
+    type: {
+        value: "all",
+        placeholder: "Type",
+        label: "Type",
+        predefinedOrder: [],
+    },
+    size: {
+        value: "all",
+        placeholder: "Size",
+        label: "Size",
+        predefinedOrder: ["Small", "Medium", "Large"],
+    },
+    neighborhood: {
+        value: "all",
+        placeholder: "Neighborhood",
+        label: "Neighborhood",
+        predefinedOrder: [],
+    },
+    purchaseRequired: {
+        value: "all",
+        placeholder: "Purchase Required",
+        label: "Purchase Required",
+        predefinedOrder: ["Yes", "No"],
+    },
+    parkingSituation: {
+        value: "all",
+        placeholder: "Parking Situation",
+        label: "Parking Situation",
+        predefinedOrder: [],
+    },
+    freeWifi: {
+        value: "all",
+        placeholder: "Free Wifi",
+        label: "Free Wifi",
+        predefinedOrder: ["Yes", "No"],
+    },
+    hasCinnamonRolls: {
+        value: "all",
+        placeholder: "Has Cinnamon Rolls",
+        label: "Has Cinnamon Rolls",
+        predefinedOrder: ["Yes", "No", "Sometimes"],
+    },
 };
