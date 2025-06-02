a6e5e6c5021f9544ce7cdc9a57112b34dce215cc
Segun Akinyemi <akintundejr4@gmail.com>
2025-06-01 21:37:59 -0400

    removing ag grid stuff and fixing reliance on it to load a specific version of IBM Plex Sans

--- charlotte-third-places/app/layout.tsx
+++ charlotte-third-places/app/layout.tsx
@@ -77,6 +77,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <GoogleTagManager gtmId="GTM-KFSPZP5P" />
       <body
