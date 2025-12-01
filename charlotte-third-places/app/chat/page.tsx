74d766c9251bb11f88a2d6e23fe377fb9343f1ac
Segun Akinyemi <akintundejr4@gmail.com>
2025-12-01 15:35:12 -0500

    try this

--- charlotte-third-places/app/chat/page.tsx
+++ charlotte-third-places/app/chat/page.tsx
@@ -4,7 +4,7 @@ import { ChatContent } from "@/components/ChatContent"
 
 export default function ChatPage() {
     return (
-        <div className="flex flex-col h-[calc(100dvh-4rem)] sm:h-auto max-w-3xl mx-auto overflow-y-auto pb-16">
+        <div className="flex flex-col h-[calc(100dvh-4rem)] sm:h-auto max-w-3xl mx-auto overflow-y-auto pb-safe">
             <ChatContent variant="page" showStarterPrompts />
         </div>
     )
